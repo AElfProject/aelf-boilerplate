2ce5fc194be3186c28343949562b4f0b3ba7ae54
samfrb <samos2x@outlook.com>
2019-04-19 18:11:05 +0800

    wip: debug sdk load exception.

---
+++ Chain/src/Contract/AElf.Contracts.MultiToken/TokenContract_Views.cs
@@ -0,0 +1,66 @@
+﻿using AElf.Contracts.MultiToken.Messages;
+using AElf.Sdk.CSharp;
+
+namespace AElf.Contracts.MultiToken
+{
+    public partial class TokenContract
+    {
+        [View]
+        public override TokenInfo GetTokenInfo(GetTokenInfoInput input)
+        {
+            return State.TokenInfos[input.Symbol];
+        }
+
+        [View]
+        public override GetBalanceOutput GetBalance(GetBalanceInput input)
+        {
+            return new GetBalanceOutput()
+            {
+                Symbol = input.Symbol,
+                Owner = input.Owner,
+                Balance = State.Balances[input.Owner][input.Symbol]
+            };
+        }
+
+        [View]
+        public override GetAllowanceOutput GetAllowance(GetAllowanceInput input)
+        {
+            return new GetAllowanceOutput()
+            {
+                Symbol = input.Symbol,
+                Owner = input.Owner,
+                Spender = input.Spender,
+                Allowance = State.Allowances[input.Owner][input.Spender][input.Symbol]
+            };
+        }
+
+        #region ForTests
+        /*
+        [View]
+        
+        public string GetTokenInfo2(string symbol)
+        {
+            return GetTokenInfo(new GetTokenInfoInput() {Symbol = symbol}).ToString();
+        }
+
+        [View]
+        public string GetBalance2(string symbol, Address owner)
+        {
+            return GetBalance(
+                new GetBalanceInput() {Symbol = symbol, Owner = owner})?.ToString();
+        }
+
+        [View]
+        public string GetAllowance2(string symbol, Address owner, Address spender)
+        {
+            return GetAllowance(new GetAllowanceInput()
+            {
+                Owner = owner,
+                Symbol = symbol,
+                Spender = spender
+            })?.ToString();
+        }
+        */
+        #endregion
+    }
+}
