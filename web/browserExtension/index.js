5f4419cfa8e5f2e52e82cf3c6728f73054f466b4
mxdai <973526962@qq.com>
2019-08-03 14:27:50 +0800

    refactor - rebuild and format code in browserExtension/index.js

--- web/browserExtension/index.js
+++ web/browserExtension/index.js
@@ -4,27 +4,23 @@
  * @description How to use the plugin for hello world
  */
 
-AElf = window.AElf;
+const { AElf } = window;
 const Wallet = AElf.wallet;
-const sha256 = AElf.utils.sha256;
+const { sha256 } = AElf.utils;
 
 // address: 65dDNxzcd35jESiidFXN5JV8Z7pCwaFnepuYQToNefSgqk9
 const defaultPrivateKey = 'bdb3b39ef4cd18c2697a920eb6d9e8c3cf1a930570beb37d04fb52400092c42b';
-
-let wallet = Wallet.getWalletByPrivateKey(defaultPrivateKey);
-const aelf = new AElf(new AElf.providers.HttpProvider(
-  'http://127.0.0.1:1235'
-));
+const wallet = Wallet.getWalletByPrivateKey(defaultPrivateKey);
+const aelf = new AElf(new AElf.providers.HttpProvider('http://127.0.0.1:1235'));
 
 if (!aelf.isConnected()) {
   alert('Blockchain Node is not running.');
 }
 
 const helloWorldContractName = 'AElf.ContractNames.HelloWorldContract';
-
 const appName = 'hello world';
-
 let nightElfInstance = null;
+
 class NightElfCheck {
   constructor() {
     const readyMessage = 'NightElf is ready';
@@ -42,10 +38,14 @@ class NightElfCheck {
       resovleTemp = resolve;
     });
     document.addEventListener('NightElf', result => {
-      console.log('test.js check the status of extension named nightElf: ', result);
+      console.log(
+        'test.js check the status of extension named nightElf: ',
+        result
+      );
       resovleTemp(readyMessage);
     });
   }
+
   static getInstance() {
     if (!nightElfInstance) {
       nightElfInstance = new NightElfCheck();
@@ -55,101 +55,116 @@ class NightElfCheck {
   }
 }
 
-const nightElfCheck = NightElfCheck.getInstance();
+function initDomEvent(helloWorldAddress) {
+  const aelfNightElf = new window.NightElf.AElf({
+    // Enter your test address in this location
+    httpProvider: ['http://127.0.0.1:1235'],
+    appName
+  });
 
-aelf.chain.getChainStatus().then(async result => {
-  const {
-    GenesisContractAddress
-  } = result;
-  const zeroC = await aelf.chain.contractAt(GenesisContractAddress, wallet);
-  const helloWorldAddress = await zeroC.GetContractAddressByName.call(sha256(helloWorldContractName));
+  let chainId = null;
+  let walletNightElf = null;
+  const getChainStatus = document.getElementById('get-chain-status');
+  const login = document.getElementById('login');
+  const initContract = document.getElementById('init');
+  const helloDom = document.getElementById('hello');
 
-  // 1.ugly way.
-  // document.addEventListener('NightElf', result => {});
-  // 2.The better way.
-  nightElfCheck.check.then(message => {
-    console.log('nightElfCheck result', message);
-    alert(message);
-
-    const aelf = new window.NightElf.AElf({
-      // Enter your test address in this location
-      httpProvider: [
-        'http://127.0.0.1:1235'
-      ],
-      appName
-    });
-
-    let chainId;
-    const getChainStatus = document.getElementById('get-chain-status');
-    getChainStatus.onclick = () => {
-      aelf.chain.getChainStatus((error, result) => {
+  getChainStatus.onclick = () => {
+    aelfNightElf.chain
+      .getChainStatus()
+      .then(res => {
         console.log('>>>>>>>>>>>>> getChainStatus >>>>>>>>>>>>>');
-        console.log(error, result);
-        chainId = result.ChainId;
+        console.log(res);
+        chainId = res.ChainId;
+      })
+      .catch(err => {
+        console.log(err);
       });
-    };
+  };
 
-    const login = document.getElementById('login');
-    login.onclick = () => {
+  login.onclick = () => {
+    if (!chainId) {
+      alert('please click getChainStatus at first');
+      return;
+    }
+    console.log('login....');
 
-      if (!chainId) {
-        alert('please click getChainStatus at first');
-        return;
-      }
-      console.log('login....');
-
-      aelf.login({
-        appName,
-        chainId: chainId,
-        payload: {
-          method: 'LOGIN',
-          contracts: [{
-            chainId: chainId,
+    const loginParameter = {
+      appName,
+      chainId,
+      payload: {
+        method: 'LOGIN',
+        contracts: [
+          {
+            chainId,
             contractAddress: helloWorldAddress,
             contractName: 'hello world',
             description: 'hello world contract',
             github: ''
-          }]
-        }
-      }, (err, result) => {
-        console.log('>>>>>>> login >>>>>>>>>>>>', err, result);
-        wallet = JSON.parse(result.detail);
-      });
-    };
-
-    const initContract = document.getElementById('init');
-    initContract.onclick = () => {
-
-      if (!wallet) {
-        alert('please click login at first');
-        return;
+          }
+        ]
       }
-
-      aelf.chain.contractAt(
-        helloWorldAddress,
-        wallet,
-        (error, result) => {
-          console.log('>>>>>>>>>>>>> contractAtAsync >>>>>>>>>>>>>');
-          console.log(error, result);
-          window.helloWorldC = result;
-        }
-      );
     };
 
-    const helloDom = document.getElementById('hello');
-    helloDom.onclick = () => {
-
-      if (!window.helloWorldC) {
-        alert('please click init contract at first');
-        return;
-      }
-
-      window.helloWorldC.Hello.call((err, result) => {
-        console.log(err, result);
-        alert(result.Value);
+    aelfNightElf.login(loginParameter)
+      .then(result => {
+        console.log('>>>>>>> login >>>>>>>>>>>>', result);
+        walletNightElf = JSON.parse(result.detail);
+      })
+      .catch(err => {
+        console.log(err);
       });
-    };
-  }).catch(error => {
-    alert(error.message);
+  };
+
+  initContract.onclick = () => {
+    if (!walletNightElf) {
+      alert('please click login at first');
+      return;
+    }
+
+    aelfNightElf.chain.contractAt(helloWorldAddress, walletNightElf)
+      .then(result => {
+        console.log('>>>>>>>>>>>>> contractAtAsync >>>>>>>>>>>>>');
+        console.log(result);
+        window.helloWorldC = result;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  helloDom.onclick = () => {
+    if (!window.helloWorldC) {
+      alert('please click init contract at first');
+      return;
+    }
+
+    window.helloWorldC.Hello.call((err, result) => {
+      console.log(err, result);
+      alert(result.Value);
+    });
+  };
+}
+
+
+aelf.chain
+  .getChainStatus()
+  .then(res => aelf.chain.contractAt(res.GenesisContractAddress, wallet))
+  .then(zeroC => zeroC.GetContractAddressByName.call(sha256(helloWorldContractName)))
+  .then(helloWorldAddress => {
+    const nightElfCheck = NightElfCheck.getInstance();
+    nightElfCheck.check
+      .then(message => {
+        // check plug-in
+        console.log('nightElfCheck result', message);
+        alert(message);
+
+        initDomEvent(helloWorldAddress);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  })
+  .catch(err => {
+    console.log(err);
   });
-});
