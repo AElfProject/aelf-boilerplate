24a60e5e9419c0d4c25c96f2a5f11a26846472f8
mx dai <mx.dai@aelf.io>
2019-09-16 10:54:14 +0800

    add randomlist method

--- chain/contract/AElf.Contracts.HelloWorldContract/HelloWorldContractState.cs
+++ chain/contract/AElf.Contracts.HelloWorldContract/HelloWorldContractState.cs
@@ -4,5 +4,11 @@ namespace AElf.Contracts.HelloWorldContract
 {
     public class HelloWorldContractState : ContractState
     {
+        public SingletonState<VisitorList> Visitors { get; set; }
+        public SingletonState<RequestRandomParam> RequestP { get; set; }
+
+        public Int64State Nonce {get;set;}
+
+        internal Acs6.RandomNumberProviderContractContainer.RandomNumberProviderContractReferenceState ACS6Contract { get; set; }
     }
 }
