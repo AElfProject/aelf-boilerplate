47c9ca89f67cd7ef4accabc02f3a183b487dddbb
mx dai <mx.dai@aelf.io>
2019-09-23 17:58:05 +0800

    reduction helloworld contract

--- chain/contract/AElf.Contracts.HelloWorldContract/HelloWorldContractState.cs
+++ chain/contract/AElf.Contracts.HelloWorldContract/HelloWorldContractState.cs
@@ -4,11 +4,5 @@ namespace AElf.Contracts.HelloWorldContract
 {
     public class HelloWorldContractState : ContractState
     {
-        public SingletonState<VisitorList> Visitors { get; set; }
-        public SingletonState<RequestRandomParam> RequestP { get; set; }
-
-        public Int64State Nonce {get;set;}
-
-        internal Acs6.RandomNumberProviderContractContainer.RandomNumberProviderContractReferenceState ACS6Contract { get; set; }
     }
 }
