5bc477e6471a86d8f7efbb792ee914e84ed6b685
yiqi <yiqi.zhao@aelf.io>
2019-11-26 15:08:20 +0800

    Clear contract projects.

--- chain/contract/AElf.Contracts.BingoGameContract/BingoGameContractConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace AElf.Contracts.BingoGameContract
-{
-    public static class BingoGameContractConstants
-    {
-        public const string CardSymbol = "CARD";
-        public const long InitialCards = 10000;
-    }
-}
--- chain/contract/AElf.Contracts.HelloWorldContract/HelloWorldContractState.cs
+++
@@ -1,8 +0,0 @@
-using AElf.Sdk.CSharp.State;
-
-namespace AElf.Contracts.HelloWorldContract
-{
-    public class HelloWorldContractState : ContractState
-    {
-    }
-}
