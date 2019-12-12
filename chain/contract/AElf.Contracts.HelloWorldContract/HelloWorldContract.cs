0b50c43ea6c9093a6d6c8ba5104a2247171490c7
samfrb <sam.bassett@aelf.io>
2019-12-12 16:45:07 +0800

    Extra comments for the hello world contract.

--- chain/contract/AElf.Contracts.HelloWorldContract/HelloWorldContract.cs
+++ chain/contract/AElf.Contracts.HelloWorldContract/HelloWorldContract.cs
@@ -2,8 +2,19 @@ using Google.Protobuf.WellKnownTypes;
 
 namespace AElf.Contracts.HelloWorldContract
 {
+    /// <summary>
+    /// The C# implementation of the contract defined in hello_world_contract.proto that is located in the "protobuf"
+    /// folder.
+    /// Notice that it inherits from the protobuf generated code. 
+    /// </summary>
     public class HelloWorldContract : HelloWorldContractContainer.HelloWorldContractBase
     {
+        /// <summary>
+        /// The implementation of the Hello method. It takes no parameters and returns on of the custom data types
+        /// defined in the protobuf definition file.
+        /// </summary>
+        /// <param name="input">Empty message (from Protobuf)</param>
+        /// <returns>a HelloReturn</returns>
         public override HelloReturn Hello(Empty input)
         {
             return new HelloReturn {Value = "Hello World!"};
--- chain/contract/AElf.Contracts.HelloWorldContract/HelloWorldContractState.cs
+++ chain/contract/AElf.Contracts.HelloWorldContract/HelloWorldContractState.cs
@@ -2,7 +2,11 @@ using AElf.Sdk.CSharp.State;
 
 namespace AElf.Contracts.HelloWorldContract
 {
+    /// <summary>
+    /// The state class of the contract, it inherits from the AElf.Sdk.CSharp.State.ContractState type. 
+    /// </summary>
     public class HelloWorldContractState : ContractState
     {
+        // state definitions go here.
     }
 }
