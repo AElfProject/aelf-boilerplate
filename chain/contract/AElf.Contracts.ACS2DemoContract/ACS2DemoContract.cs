59c7cc872f7b23f21268dc458ac3a22267d3aa43
yiqi <yiqi.zhao@aelf.io>
2020-05-11 21:34:52 +0800

    Fix proto files.

--- chain/contract/AElf.Contracts.ACS2DemoContract/ACS2DemoContract.cs
+++ chain/contract/AElf.Contracts.ACS2DemoContract/ACS2DemoContract.cs
@@ -2,7 +2,7 @@ using Acs2;
 using AElf.Sdk.CSharp;
 using Google.Protobuf.WellKnownTypes;
 
-namespace AElf.Contracts.ACS1DemoContract
+namespace AElf.Contracts.ACS2DemoContract
 {
     /// <summary>
     /// ACS1 can only used by system contracts.
--- chain/contract/AElf.Contracts.ACS3DemoContract/ACS3DemoContractState.cs
+++ chain/contract/AElf.Contracts.ACS3DemoContract/ACS3DemoContractState.cs
@@ -5,7 +5,5 @@ namespace AElf.Contracts.ACS3DemoContract
 {
     public class ACS3DemoContractState : ContractState
     {
-        public SingletonState<AuthorityInfo> MethodFeeController { get; set; }
-        public MappedState<string, MethodFees> TransactionFees { get; set; }
     }
 }
