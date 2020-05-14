e6f902a4b34105c9f96d4daf68e1703b266b358c
yiqi <yiqi.zhao@aelf.io>
2020-05-14 15:55:53 +0800

    Demo for acs2.

--- chain/contract/AElf.Contracts.ACS2DemoContract/ACS2DemoContract.cs
+++ chain/contract/AElf.Contracts.ACS2DemoContract/ACS2DemoContract.cs
@@ -1,4 +1,5 @@
 using Acs2;
+using AElf.CSharp.Core;
 using AElf.Sdk.CSharp;
 using AElf.Types;
 using Google.Protobuf.WellKnownTypes;
@@ -10,9 +11,46 @@ namespace AElf.Contracts.ACS2DemoContract
     /// </summary>
     public class ACS2DemoContract : ACS2DemoContractContainer.ACS2DemoContractBase
     {
+        public override Empty TransferCredits(TransferCreditsInput input)
+        {
+            var remainCredits = State.Credits[Context.Sender];
+            Assert(remainCredits >= input.Amount, "Insufficient balance.");
+            State.Credits[Context.Sender] = remainCredits.Sub(input.Amount);
+            State.Credits[input.To] = State.Credits[input.To].Add(input.Amount);
+            return new Empty();
+        }
+
         public override ResourceInfo GetResourceInfo(Transaction input)
         {
-            return base.GetResourceInfo(input);
+            if (input.MethodName == nameof(TransferCredits))
+            {
+                var args = TransferCreditsInput.Parser.ParseFrom(input.Params);
+                return new ResourceInfo
+                {
+                    Paths =
+                    {
+                        GetPath(nameof(ACS2DemoContractState.Credits), input.From.ToString()),
+                        GetPath(nameof(ACS2DemoContractState.Credits), args.To.ToString()),
+                    }
+                };
+            }
+
+            return new ResourceInfo {NonParallelizable = true};
+        }
+
+        private ScopedStatePath GetPath(params string[] parts)
+        {
+            return new ScopedStatePath
+            {
+                Address = Context.Self,
+                Path = new StatePath
+                {
+                    Parts =
+                    {
+                        parts
+                    }
+                }
+            };
         }
     }
 }
