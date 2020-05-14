8f82c556d8c32c3fb38f57c310c03f904dce8030
yiqi <yiqi.zhao@aelf.io>
2020-05-14 22:08:21 +0800

    Demo for acs5.

--- chain/contract/AElf.Contracts.ACS5DemoContract/ACS5DemoContractState.cs
+++ chain/contract/AElf.Contracts.ACS5DemoContract/ACS5DemoContractState.cs
@@ -1,9 +1,12 @@
 using Acs5;
 using AElf.Sdk.CSharp.State;
+using AElf.Types;
 
 namespace AElf.Contracts.ACS5DemoContract
 {
     public class ACS5DemoContractState : ContractState
     {
+        public SingletonState<Address> Admin { get; set; }
+        public MappedState<string, MethodCallingThreshold> MethodCallingThresholds { get; set; }
     }
 }
