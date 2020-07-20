00c604350d1967d5cc934386d2aaf2f04921ff07
jinpeng wang <jason.wang@hoopox.com>
2020-07-20 15:47:14 +0800

    Add FinanceContract

---
+++ chain/contract/AElf.Contracts.FinanceContract/FinanceContractState.cs
@@ -0,0 +1,141 @@
+using AElf.Contracts.Genesis;
+using AElf.Contracts.MultiToken;
+using AElf.Sdk.CSharp.State;
+using AElf.Types;
+
+namespace AElf.Contracts.FinanceContract
+{
+    public class FinanceContractState : ContractState
+    {
+        internal TokenContractContainer.TokenContractReferenceState TokenContract { get; set; }
+        
+        internal BasicContractZeroContainer.BasicContractZeroReferenceState GenesisContract { get; set; }
+
+        /// <summary>
+        /// Contract administrator
+        /// </summary>
+        public StringState Admin { get; set; }
+        
+        /// <summary>
+        /// Contract pending administrator
+        /// </summary>
+        public StringState PendingAdmin { get; set; }
+        
+        /// <summary>
+        /// Market administrator
+        /// </summary>
+        public MappedState<string, Address> Admins { get; set; }
+
+        /// <summary>
+        /// Pending administrator
+        /// </summary>
+        public MappedState<string, Address> PendingAdmins { get; set; }
+
+        /// <summary>
+        /// Block number that interest was last accrued at
+        /// </summary>
+        public MappedState<string, long> AccrualBlockNumbers { get; set; }
+
+        /// <summary>
+        /// Total amount of outstanding borrows of the underlying in this market
+        /// </summary>
+        public MappedState<string, long> TotalBorrows { get; set; }
+
+        /// <summary>
+        /// Total amount of reserves of the underlying held in this market
+        /// </summary>
+        public MappedState<string, long> TotalReserves { get; set; }
+        
+        /// <summary>
+        /// Total number of tokens in circulation(CToken)
+        /// </summary>
+        public MappedState<string, long> TotalSupply { get; set; }
+
+        /// <summary>
+        /// Accumulator of the total earned interest rate since the opening of the market
+        /// </summary>
+        public MappedState<string, string> BorrowIndex { get; set; }
+
+        /// <summary>
+        /// Mapping of account addresses to outstanding borrow balances
+        /// </summary>
+        public MappedState<string, Address, BorrowSnapshot> AccountBorrows { get; set; }
+
+        /// <summary>
+        /// Initial exchange rate used when minting the first CTokens (used when totalSupply = 0)
+        /// </summary>
+        /// <returns></returns>
+        public MappedState<string, string> InitialExchangeRate { get; set; }
+
+        /// <summary>
+        /// Fraction of interest currently set aside for reserves
+        /// </summary>
+        public MappedState<string, string> ReserveFactor { get; set; }
+
+        /// <summary>
+        /// Token balances for each account(CToken)
+        /// </summary>
+        public MappedState<string, Address, long> AccountTokens { get; set; }
+        
+        /// <summary>
+        /// Market metadata
+        /// </summary>
+        public MappedState<string, Market> Markets { get; set; }
+        
+        /// <summary>
+        /// Per-account mapping of "assets you are in", capped by maxAssets
+        /// </summary>
+        public MappedState<Address,AssetList> AccountAssets { get; set; }
+        
+        
+        /// <summary>
+        /// Multiplier used to calculate the maximum repayAmount when liquidating a borrow
+        /// </summary>
+        /// <returns></returns>
+        public StringState CloseFactor { get; set; }
+
+        /// <summary>
+        /// Multiplier representing the discount on collateral that a liquidator receives
+        /// </summary>
+        /// <returns></returns>
+        public StringState LiquidationIncentive { get; set; }
+
+        /// <summary>
+        /// Max number of assets a single account can participate in (borrow or use as collateral)
+        /// </summary>
+        /// <returns></returns>
+        public Int32State MaxAssets { get; set; }
+        
+        /// <summary>
+        /// A list of all markets
+        /// </summary>
+        public SingletonState<SymbolList> AllMarkets { get; set; }
+
+        /**
+        *  The Pause Guardian can pause certain actions as a safety mechanism.
+        *  Actions which allow users to remove their own assets cannot be paused.
+        *  Liquidation / seizing / transfer can only be paused globally, not by market.
+        */
+        public SingletonState<Address> PauseGuardian { get; set; }
+        
+        public BoolState TransferGuardianPaused { get; set; }
+        
+        public BoolState SeizeGuardianPaused { get; set; }
+        
+        public MappedState<string,bool> MintGuardianPaused { get; set; }
+        
+        public MappedState<string,bool> BorrowGuardianPaused { get; set; }
+        
+        public MappedState<string,long> Prices { get; set; }
+
+        /// <summary>
+        /// The base interest rate which is the y-intercept when utilization rate is 0
+        /// </summary>
+        public MappedState<string, string> BaseRatePerBlock { get; set; }
+
+        /// <summary>
+        /// The multiplier of utilization rate that gives the slope of the interest rate
+        /// </summary>
+        public MappedState<string, string> MultiplierPerBlock { get; set; }
+    }
+}
---
+++ chain/contract/AElf.Contracts.FinanceContract/FinanceContract_Constants.cs
@@ -0,0 +1,37 @@
+namespace AElf.Contracts.FinanceContract
+{
+    public partial class FinanceContract
+    {
+        /// <summary>
+        /// Maximum borrow rate that can ever be applied (.0005% / block)
+        /// </summary>
+        public const long MaxBorrowRate = 500_000_000_000_000;
+
+        /// <summary>
+        /// Maximum fraction of interest that can be set aside for reserves
+        /// </summary>
+        public const long MaxReserveFactor = 1000_000_000_000_000_000;
+        
+        /// <summary>
+        /// The approximate number of blocks per year that is assumed by the interest rate model
+        /// </summary>
+        public const int BlocksPerYear = 63072000; //2 * 60 * 60 * 24 * 365
+        
+        // closeFactorMantissa must be strictly greater than this value
+        public const string MinCloseFactor= "0.05"; // 0.05
+
+        // closeFactorMantissa must not exceed this value
+        public const string MaxCloseFactor = "0.9"; // 0.9
+
+        // No collateralFactorMantissa may exceed this value
+        public const string MaxCollateralFactor = "0.9"; // 0.9
+
+        // liquidationIncentiveMantissa must be no less than this value
+        public const string MinLiquidationIncentive = "1"; // 1.0
+
+        // liquidationIncentiveMantissa must be no greater than this value
+        public const string MaxLiquidationIncentive = "1.5"; // 1.5
+
+        public const int CTokenDecimals = 18;
+    }
+}
