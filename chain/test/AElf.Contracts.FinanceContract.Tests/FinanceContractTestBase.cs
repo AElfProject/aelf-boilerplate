84128b6eb17edb5c7a7e93f093ce49f2c42faa59
solnex <yiyuan.liu@aelf.io>
2020-07-31 00:23:15 +0800

    add  some tests for FinanceContract

--- chain/test/AElf.Contracts.FinanceContract.Tests/FinanceContractTestBase.cs
+++ chain/test/AElf.Contracts.FinanceContract.Tests/FinanceContractTestBase.cs
@@ -7,6 +7,15 @@ using AElf.Kernel;
 using AElf.Kernel.SmartContract.Application;
 using AElf.Types;
 using Google.Protobuf;
+using System.Linq;
+using AElf.Boilerplate.TestBase;
+using AElf.Contracts.MultiToken;
+using AElf.Cryptography.ECDSA;
+using AElf.Kernel;
+using AElf.Kernel.Blockchain.Application;
+using AElf.Kernel.SmartContract.Application;
+using AElf.Kernel.Token;
+using AElf.Types;
 using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Threading;
 
@@ -15,19 +24,38 @@ namespace AElf.Contracts.FinanceContract
     public class FinanceContractTestBase : ContractTestBase<FinanceContractTestModule>
     {
         internal Address FinanceContractAddress;
+        internal TokenContractContainer.TokenContractStub UserTokenContractStub => GetTokenContractStub(UserKeyPair);
+        internal Address tokenContractAddress => GetAddress(TokenSmartContractAddressNameProvider.StringName);
+
         internal FinanceContractContainer.FinanceContractStub GetFinanceContractStub(ECKeyPair senderKeyPair)
         {
             return GetTester<FinanceContractContainer.FinanceContractStub>(FinanceContractAddress, senderKeyPair);
         }
-        
+
+        internal FinanceContractContainer.FinanceContractStub FinanceContractStub =>
+            GetFinanceContractStub(SampleAccount.Accounts.First().KeyPair);
+
+        internal FinanceContractContainer.FinanceContractStub UserStub =>
+            GetFinanceContractStub(UserKeyPair);
+
+        internal TokenContractContainer.TokenContractStub TokenContractStub =>
+            GetTokenContractStub(SampleAccount.Accounts.First().KeyPair);
+
+        internal TokenContractContainer.TokenContractStub GetTokenContractStub(ECKeyPair senderKeyPair)
+        {
+            return Application.ServiceProvider.GetRequiredService<IContractTesterFactory>()
+                .Create<TokenContractContainer.TokenContractStub>(tokenContractAddress, senderKeyPair);
+        }
+
         // You can get address of any contract via GetAddress method, for example:
         // internal Address DAppContractAddress => GetAddress(DAppSmartContractAddressNameProvider.StringName);
         public FinanceContractTestBase()
         {
-            FinanceContractAddress= AsyncHelper.RunSync(()=>DeployContractAsync(KernelConstants.DefaultRunnerCategory,
-                File.ReadAllBytes(typeof( FinanceContract).Assembly.Location), SampleAccount.Accounts[0].KeyPair));
+            FinanceContractAddress = AsyncHelper.RunSync(() => DeployContractAsync(
+                KernelConstants.DefaultRunnerCategory,
+                File.ReadAllBytes(typeof(FinanceContract).Assembly.Location), SampleAccount.Accounts[0].KeyPair));
         }
-        
+
         private async Task<Address> DeployContractAsync(int category, byte[] code, ECKeyPair keyPair)
         {
             var addressService = Application.ServiceProvider.GetRequiredService<ISmartContractAddressService>();
@@ -40,7 +68,21 @@ namespace AElf.Contracts.FinanceContract
             });
             return executionResult.Output;
         }
-    }
-   
 
+        internal ECKeyPair UserKeyPair { get; set; } = SampleAccount.Accounts.Last().KeyPair;
+        internal Address UserAddress => Address.FromPublicKey(UserKeyPair.PublicKey);
+
+        private Address GetAddress(string contractName)
+        {
+            var addressService = Application.ServiceProvider.GetRequiredService<ISmartContractAddressService>();
+            var blockChainService = Application.ServiceProvider.GetRequiredService<IBlockchainService>();
+            var chain = AsyncHelper.RunSync(blockChainService.GetChainAsync);
+            var address = AsyncHelper.RunSync(() => addressService.GetSmartContractAddressAsync(new ChainContext()
+            {
+                BlockHash = chain.BestChainHash,
+                BlockHeight = chain.BestChainHeight
+            }, contractName)).SmartContractAddress.Address;
+            return address;
+        }
+    }
 }
--- chain/test/AElf.Contracts.FinanceContract.Tests/FinanceContractTests.cs
+++ chain/test/AElf.Contracts.FinanceContract.Tests/FinanceContractTests.cs
@@ -1,6 +1,10 @@
 using System.Linq;
 using System.Threading.Tasks;
 using AElf.ContractTestBase.ContractTestKit;
+using AElf.Contracts.MultiToken;
+using AElf.Types;
+using Google.Protobuf;
+using Google.Protobuf.Collections;
 using Google.Protobuf.WellKnownTypes;
 using Shouldly;
 using Xunit;
@@ -12,36 +16,134 @@ namespace AElf.Contracts.FinanceContract
         [Fact]
         public async Task Test()
         {
-            // Get a stub for testing.
-            var keyPair = SampleAccount.Accounts.First().KeyPair;
-            var stub = GetFinanceContractStub(keyPair);
-           await stub.Initialize.SendAsync(new InitializeInput()
+            await FinanceContractStub.Initialize.SendAsync(new InitializeInput()
             {
                 CloseFactor = "0.1",
-                LiquidationIncentive = "0.1",
+                LiquidationIncentive = "1.1",
                 MaxAssets = 5
             });
-           var result= await stub.GetCloseFactor.SendAsync(new Empty());
+           var result= await FinanceContractStub.GetCloseFactor.SendAsync(new Empty());
            result.Output.Value.ShouldBe("0.1");
-
-           await  stub.SupportMarket.SendAsync(new SupportMarketInput()
+           
+           await  FinanceContractStub.SupportMarket.SendAsync(new SupportMarketInput()
            {
                BaseRatePerBlock = "0.0001",
                InitialExchangeRate = "1.1",
                MultiplierPerBlock = "0.0001",
-               ReserveFactor = "0.0001",
-               Symbol = "Elf"
+               ReserveFactor = "0.001",
+               Symbol = "ELF"
            });
-          await stub.Mint.SendAsync(new MintInput()
+     
+          await TokenContractStub.Transfer.SendAsync(new TransferInput()
            {
-               Amount = 500, Symbol = "Elf"
+               Amount = 100000,
+               Symbol = "ELF",
+               Memo = "Recharge",
+               To = UserAddress
            });
-        var snapshotOutput=  await stub.GetAccountSnapshot.CallAsync(new Account()
+         var banlance= await TokenContractStub.GetBalance.SendAsync(new GetBalanceInput()
+          {
+              Owner = UserAddress,
+              Symbol = "ELF"
+          });
+         banlance.Output.Balance.ShouldBe(100000);
+         await UserTokenContractStub.Approve.SendAsync(new ApproveInput()
+         {
+             Amount = 100000,
+             Spender = FinanceContractAddress,
+             Symbol = "ELF"
+         });
+         await UserStub.Mint.SendAsync(new MintInput()
            {
-               Address = financeContractAddress,
-               Symbol = "Elf"
+               Amount = 500, Symbol = "ELF"
            });
-        snapshotOutput.CTokenBalance.ShouldBeNull();
+         var snapshotOutput= await UserStub.GetAccountSnapshot.SendAsync(new Account()
+           {
+               Address = UserAddress,
+               Symbol = "ELF"
+           });
+         snapshotOutput.Output.CTokenBalance.ShouldBeGreaterThan(0);
+         await UserStub.EnterMarkets.SendAsync(new EnterMarketsInput()
+         {
+             Symbols = { "ELF"}
+         });
+           var BorrowBalance= await  UserStub.GetBorrowBalanceStored.SendAsync(new Account()
+         {
+             Address = UserAddress,
+             Symbol = "ELF"
+         });
+        BorrowBalance.Output.Value.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public  async Task InitializeTest()
+        {
+            await FinanceContractStub.Initialize.SendAsync(new InitializeInput()
+            {
+                CloseFactor = "0.1",
+                LiquidationIncentive = "1.1",
+                MaxAssets = 5
+            });
+            var CloseFactor= await FinanceContractStub.GetCloseFactor.SendAsync(new Empty());
+            CloseFactor.Output.Value.ShouldBe("0.1");
+            var LiquidationIncentive= await FinanceContractStub.GetLiquidationIncentive.SendAsync(new Empty());
+            LiquidationIncentive.Output.Value.ShouldBe("1.1");
+            var MaxAssets= await FinanceContractStub.GetMaxAssets.SendAsync(new Empty());
+            MaxAssets.Output.Value.ShouldBe(5);
+        }
+       [Fact]
+        public async Task SupportMarketTest()
+        {
+            Initialize();
+            //UNAUTHORIZED
+            var result=  await  UserStub.SupportMarket.SendWithExceptionAsync(new SupportMarketInput()
+            {
+                BaseRatePerBlock = "0.0001",
+                InitialExchangeRate = "1.1",
+                MultiplierPerBlock = "0.0001",
+                ReserveFactor = "0.001",
+                Symbol = "ELF"
+            });
+            result.TransactionResult.Error.ShouldContain("UNAUTHORIZED");
+            //success
+           await  FinanceContractStub.SupportMarket.SendAsync(new SupportMarketInput()
+            {
+                BaseRatePerBlock = "0.0001",
+                InitialExchangeRate = "1.1",
+                MultiplierPerBlock = "0.0001",
+                ReserveFactor = "0.001",
+                Symbol = "ELF"
+            });
+           var interestRate= await FinanceContractStub.GetInterestRate.CallAsync(new StringValue()
+           {
+               Value = "ELF"
+           });
+           interestRate.BaseRatePerBlock.ShouldBe("0.0001");
+           interestRate.MultiplierPerBlock.ShouldBe("0.0001");
+          var reserve=await FinanceContractStub.GetReserveFactor.CallAsync(new StringValue()
+           {
+               Value = "ELF"
+           });
+          reserve.Value.ShouldBe("0.001");
+           //MARKET_EXISTS
+         var  exception=   await  FinanceContractStub.SupportMarket.SendWithExceptionAsync(new SupportMarketInput()
+           {
+               BaseRatePerBlock = "0.0001",
+               InitialExchangeRate = "1.1",
+               MultiplierPerBlock = "0.0001",
+               ReserveFactor = "0.001",
+               Symbol = "ELF"
+           });
+         exception.TransactionResult.Error.Contains("MARKET_EXISTS").ShouldBe(true);
+        }
+        private async void Initialize()
+        {
+            await FinanceContractStub.Initialize.SendAsync(new InitializeInput()
+            {
+                CloseFactor = "0.1",
+                LiquidationIncentive = "1.1",
+                MaxAssets = 5
+            });
         }
     }
 }
