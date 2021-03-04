6316b785d850264f5125c5cd23ba85951f26d837
wei li <weili.li@aelf.io>
2021-03-04 10:00:17 +0800

    add node manager

---
+++ chain/contract/AElf.Contracts.OracleContract/OracleContract_Constant.cs
@@ -0,0 +1,9 @@
+namespace AElf.Contracts.OracleContract
+{
+    public partial class OracleContract
+    {
+        public const string TokenSymbol = "AELINK";
+
+        public const long DefaultClearRedundantRevenue = 10000;
+    }
+}
