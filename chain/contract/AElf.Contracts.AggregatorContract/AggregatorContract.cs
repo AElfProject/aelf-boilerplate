5aed18d0dfeccf54e7ae627e6a3e2bba7d5e8324
yiqi <yiqi.zhao@aelf.io>
2021-03-11 15:30:34 +0800

    Add Aggregator Contract.

---
+++ chain/contract/AElf.Contracts.AggregatorContract/AggregatorContract.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Google.Protobuf.WellKnownTypes;
+
+namespace AElf.Contracts.AggregatorContract
+{
+    public partial class AggregatorContract : AggregatorContractContainer.AggregatorContractBase
+    {
+        public override Empty Initialize(Empty input)
+        {
+            return base.Initialize(input);
+        }
+    }
+}
---
+++ chain/contract/AElf.Contracts.AggregatorContract/AggregatorContractState.cs
@@ -0,0 +1,9 @@
+using AElf.Sdk.CSharp.State;
+
+namespace AElf.Contracts.AggregatorContract
+{
+    public partial class AggregatorContractState : ContractState
+    {
+        
+    }
+}
