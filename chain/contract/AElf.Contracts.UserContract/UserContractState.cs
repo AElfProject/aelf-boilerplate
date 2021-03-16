8cdd1f6ac3567d6b432c85ba0cf23c8356c526c0
yiqi <yiqi.zhao@aelf.io>
2021-03-16 18:34:41 +0800

    Add User Contract for testing.

---
+++ chain/contract/AElf.Contracts.UserContract/UserContractState.cs
@@ -0,0 +1,12 @@
+﻿using AElf.Contracts.OracleContract;
+using AElf.Sdk.CSharp.State;
+
+namespace AElf.Contracts.UserContract
+{
+    public class UserContractState : ContractState
+    {
+        internal OracleContractContainer.OracleContractReferenceState OracleContract { get; set; }
+
+        public SingletonState<TemperatureRecordList> TemperatureRecordList { get; set; }
+    }
+}
